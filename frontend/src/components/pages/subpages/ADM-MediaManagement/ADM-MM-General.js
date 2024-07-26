92d184ab588bc72fbbb7340e9f067814b25a3613
Carl Raven Barajas <56469543+ravenbarajas@users.noreply.github.com>
2024-07-26 13:06:11 +0800

    Configure Table Row Limit and Pagination

--- frontend/src/components/pages/subpages/ADM-MediaManagement/ADM-MM-General.js
+++ frontend/src/components/pages/subpages/ADM-MediaManagement/ADM-MM-General.js
@@ -10,77 +10,121 @@ const General = () => {
     const [filteredTableData, setFilteredTableData] = useState([]);
     
     const [isLoading, setIsLoading] = useState(true); // To handle loading state
-    const [isEmpty, setIsEmpty] = useState(false); // To handle empty state
+    const [isEmpty, setIsEmpty] = useState(false);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const rowsPerPage = 10;
 
     const [searchQuery, setSearchQuery] = useState('');
     const [mediaData, setMediaData] = useState([]); // To store fetched media data
 
     useEffect(() => {
-        // Fetch data when component mounts or when the page is revisited
         const fetchData = async () => {
             try {
                 const response = await fetch('http://localhost:8000/api/media');
                 const data = await response.json();
-                
-                setHeaders(Object.keys(data[0] || {}));
+
+                const dataHeaders = Object.keys(data[0] || {});
+                const filteredHeaders = dataHeaders.filter(col => columnMapping[col]);
+
+                setHeaders(filteredHeaders.map(col => columnMapping[col]));
                 setTableData(data);
-                setIsEmpty(data.length === 0); // Check if data is empty
+                setIsEmpty(data.length === 0);
             } catch (error) {
                 console.error('Error fetching table data', error);
             } finally {
-                setIsLoading(false); // Set loading to false after fetching
+                setIsLoading(false);
             }
         };
 
         fetchData();
-    }, []); // Empty dependency array means this runs once when the component mounts
+    }, []);
+    
+    
+    const indexOfLastRow = currentPage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentTableData = tableData.slice(indexOfFirstRow, indexOfLastRow);
 
-    const handleFileChange = (e) => {
-        const selectedFile = e.target.files[0];
-      
-        if (selectedFile) {
-            const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
-            const allowedExtensions = ['xlsx', 'xls', 'csv'];
-            
-            if (!allowedExtensions.includes(fileExtension)) {
-                alert('Only .xlsx, .xls, and .csv files are supported. Please choose a valid file.');
-                return;
-            }
+    const totalPages = Math.ceil(tableData.length / rowsPerPage);
 
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const data = new Uint8Array(event.target.result);
-                const workbook = XLSX.read(data, { type: 'array' });
-                const sheetName = workbook.SheetNames[0];
-                const worksheet = workbook.Sheets[sheetName];
-                const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-                const sheetHeaders = jsonData[0];
-                const dataRows = jsonData.slice(1);
-
-                const formattedData = dataRows.map((row) =>
-                    sheetHeaders.reduce((obj, key, index) => {
-                        obj[key] = row[index];
-                        return obj;
-                    }, {})
-                );
-
-                setHeaders(sheetHeaders);
-
-                const nonEmptyRows = formattedData.filter((row) =>
-                    Object.values(row).some((cell) => cell !== undefined && cell !== null && cell !== "")
-                );
-
-                setTableData(nonEmptyRows);
-                setFilteredTableData(nonEmptyRows);
-
-                setFileUploaded(selectedFile);
-            };
-
-            reader.readAsArrayBuffer(selectedFile);
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
         }
     };
 
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleFirstPage = () => {
+        setCurrentPage(1);
+    };
+
+    const handleLastPage = () => {
+        setCurrentPage(totalPages);
+    };
+
+    const columnMapping = {
+        catalogNumber: 'Catalog Number',
+        ivoryMusicUPCNumber: 'UPC Number',
+        albumOrDigitalSingle: 'Album / Digital Single',
+        isrcFormat: 'ISRC Format',
+        songTitles: 'Song Titles',
+        trackSequence: 'Track Sequence',
+        trackPrimaryArtistName: 'Artist Name',
+        releaseType: 'Release Type',
+        label: 'Label',
+        songVersion: 'Song Version',
+        songGenre: 'Genre',
+        trackLanguage: 'Language',
+        trackParentalAdvisory: 'Parental Advisory',
+        releasingTerritories: 'Releasing Territories',
+        excludedTerritories: 'Excluded Territories',
+        originalReleaseDate: 'Original Release Date',
+        recordingLocation: 'Recording Location',
+        trackRecordingYear: 'Recording Year',
+        publisher: 'Publisher',
+        composers: 'Composers',
+        producer: 'Producer',
+        length: 'Length',
+        notes: 'Notes',
+    };
+    
+    const handleFileChange = async (e) => {
+        const file = e.target.files[0];
+        if (!file) {
+            return;
+        }
+    
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const csvData = event.target.result;
+            const lines = csvData.split('\n');
+            const result = [];
+            const headers = lines[0].split(',');
+    
+            for (let i = 1; i < lines.length; i++) {
+                const obj = {};
+                const currentLine = lines[i].split(',');
+    
+                for (let j = 0; j < headers.length; j++) {
+                    obj[headers[j]] = currentLine[j];
+                }
+                result.push(obj);
+            }
+    
+            const filteredHeaders = headers.filter(col => columnMapping[col]);
+    
+            setHeaders(filteredHeaders.map(col => columnMapping[col]));
+            setTableData(result);
+            setFileUploaded(true);
+        };
+        reader.readAsText(file);
+    };
+    
     const handleSave = async () => {
         try {
             if (!fileUploaded) {
@@ -155,41 +199,60 @@ const General = () => {
                             </div>
                         )}
                 </div>
+                <div className='ADM-MM-General-page-ctrl'>
+                    <div className='pagination-controls'>
+                        <button className="btn-firstpage" onClick={handleFirstPage} disabled={currentPage === 1}>
+                            <i className="fa-solid fa-backward-fast"></i>
+                        </button>
+                        <button className="btn-prevpage" onClick={handlePreviousPage} disabled={currentPage === 1}>
+                            <i className="fa-solid fa-caret-left"></i>
+                        </button>
+                        <span>Page {currentPage} of {totalPages}</span>
+                        <button className="btn-nextpage" onClick={handleNextPage} disabled={currentPage === totalPages}>
+                            <i className="fa-solid fa-caret-right"></i>
+                        </button>
+                        <button className="btn-lastpage" onClick={handleLastPage} disabled={currentPage === totalPages}>
+                            <i className="fa-solid fa-forward-fast"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
             <div className='ADM-MM-General-body'>
                 <div className='ADM-MM-General-tbl-section'>
-                    {isLoading ? (
-                        <div className='loading-indicator'>Loading...</div>
-                    ) : isEmpty ? (
-                        <div className='no-data-indicator'>No data available</div>
-                    ) : (
-                    <table className='gen-media-tbl'>
-                        <thead>
-                            <tr>
-                                {headers.map((header, index) => (
-                                    <th key={index}>{header}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {tableData.map((row, rowIndex) => (
-                                <tr key={rowIndex}>
-                                    {headers.map((header, colIndex) => (
-                                        <td key={colIndex}>{row[header]}</td>
+                    <div className='ADM-MM-General-tbl-container'>
+                        {isLoading ? (
+                            <div className='loading-indicator'>Loading...</div>
+                        ) : isEmpty ? (
+                            <div className='no-data-indicator'>No data available</div>
+                        ) : (
+                        <table className='gen-media-tbl'>
+                            <thead>
+                                <tr>
+                                    {headers.map((header, index) => (
+                                        <th key={index}>{header}</th>
                                     ))}
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    )}
-                </div>
-                <div className='ADM-MM-General-ctrl-section'>
-                    
+                            </thead>
+                            <tbody>
+                                {currentTableData.map((row, rowIndex) => (
+                                    <tr key={rowIndex}>
+                                        {headers.map((header, colIndex) => (
+                                            <td key={colIndex}>
+                                                {row[Object.keys(columnMapping).find(key => columnMapping[key] === header)]}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        )}
+                    </div>
+
                 </div>
             </div>
             <div className='ADM-MM-General-footer'>
                 
-            </div>
+                </div>
         </div>
     );
 };
